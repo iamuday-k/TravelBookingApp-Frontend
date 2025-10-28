f13fc7e2a3e179e5696cfdfa6af5634c58e096d7
Uday Kumar <iamuday.k@gmail.com>
2025-10-29 00:57:23 +0530

    pushed constants directory

---
+++ constants/icons.js
@@ -0,0 +1,27 @@
+import home from "../assets/icons/home.png";
+import mdi_eye from "../assets/icons/mdi_eye.png";
+import menu from "../assets/icons/menu.png";
+import next_arrow from "../assets/icons/next-arrow.png";
+import profile from "../assets/icons/profile.png";
+import rewards from "../assets/icons/rewards.png";
+import right_arrow from "../assets/icons/right-arrow.png";
+import search from "../assets/icons/search.png";
+import share from "../assets/icons/share.png";
+import start_arrow from "../assets/icons/start-arrow.png";
+import trips from "../assets/icons/trips.png";
+import wishlist from "../assets/icons/wishlist.png";
+
+export default {
+  home,
+  mdi_eye,
+  menu,
+  next_arrow,
+  profile,
+  rewards,
+  right_arrow,
+  search,
+  share,
+  start_arrow,
+  trips,
+  wishlist,
+};
---
+++ constants/images.js
@@ -0,0 +1,29 @@
+import elite_agency from "../assets/images/elite-agency.png";
+import flight from "../assets/images/flight.png";
+import login from "../assets/images/login.png";
+import logo from "../assets/images/logo.png";
+import onboard1 from "../assets/images/onboard1.png";
+import onboard2 from "../assets/images/onboard2.png";
+import onboard3 from "../assets/images/onboard3.png";
+import premium_agency from "../assets/images/premium-agency.png";
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.jpg";
+import tour from "../assets/images/tour.png";
+import verified_agency from "../assets/images/verified-agency.png";
+import welcome from "../assets/images/welcome.png";
+
+export default {
+  elite_agency,
+  flight,
+  login,
+  logo,
+  onboard1,
+  onboard2,
+  onboard3,
+  premium_agency,
+  profile,
+  thumbnail,
+  tour,
+  verified_agency,
+  welcome,
+};
